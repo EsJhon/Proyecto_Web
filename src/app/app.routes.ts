fba8a5da4f13115a6be0a4acb1a264acac9bc3a4
EstudianteUC <EstudianteUC@UCUXA40118.continental.edu.pe>
2025-10-06 15:09:03 -0500

    Primer commit - V.1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Products } from './components/products/products';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'products', component: Products },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+];
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.html',
+  styleUrls: ['./home.css']
+})
+export class Home {
+    public banner = 'assets/images/banners/img_001.jpg';
+
+}
